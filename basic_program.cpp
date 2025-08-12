6c4557360373c5fd2fb3f592b561f2d7d7d21e13
mongooseintown <khaledbinnasir1714412140@gmail.com>
2025-08-12 11:34:21 +0600

    Added fibonacci function

--- basic_program.cpp
+++ basic_program.cpp
@@ -5,11 +5,17 @@ unsigned long long factorial(int n)
     if (n == 0 || n == 1) return 1;
     return n * factorial(n - 1);
 }
+unsigned long long fibonacci(int n)
+{
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
 int main()
 {
     cout << "Hello, World!" << endl;
     int num;
     cin>>num;
     cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    cout << "Fibonacci of " << num << " is " << fibonacci(num) << endl;
     return 0;
 }

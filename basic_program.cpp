1af146116f62cef3e3194f535edc8c29015c7c3e
mongooseintown <khaledbinnasir1714412140@gmail.com>
2025-08-12 11:13:42 +0600

    Added factorial function

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
+unsigned long long factorial(int n)
+{
+    if (n == 0 || n == 1) return 1;
+    return n * factorial(n - 1);
+}
 int main()
 {
     cout << "Hello, World!" << endl;
+    int num;
+    cin>>num;
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
     return 0;
 }
